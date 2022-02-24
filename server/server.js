6376e13fc44996d6f58f6e8102f6095e40a9f4af
Meke Finau <lonifinau.09@gmail.com>
2022-02-24 14:38:00 -0700

    update

--- server/server.js
+++ server/server.js
@@ -1,13 +1,29 @@
 const express = require('express')
 const path = require('path')
 
+// include and initialize the rollbar library with your access token
+var Rollbar = require("rollbar");
+var rollbar = new Rollbar({
+  accessToken: `${process.env.ACCESS_TOKEN}`,
+  captureUncaught: true,
+  captureUnhandledRejections: true
+});
+
+// record a generic message and send it to Rollbar
+
+rollbar.log("Malo e lelei!");
+
 const app = express()
+
+app.use(express.json)
 
 app.get('/', function(req, res) {
     res.sendFile(path.join(__dirname, '../index.html'))
 })
 
 app.use(express.static(path.join(__dirname, '../')))
+
+app.use(rollbar.errorHandler())
 
 const port = process.env.PORT || 4440
 
