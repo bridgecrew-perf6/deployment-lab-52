de4c566dff35faba4ed9367800170feb8ab60e96
Meke Finau <lonifinau.09@gmail.com>
2022-02-24 15:32:53 -0700

    final

--- server/server.js
+++ server/server.js
@@ -1,12 +1,15 @@
+require("dotenv").config()
+
 const express = require('express')
 const path = require('path')
+
 
 // include and initialize the rollbar library with your access token
 var Rollbar = require("rollbar");
 var rollbar = new Rollbar({
-  accessToken: '08e032e9980b48f5ac3160d96e7d3172', //access token
+  accessToken: process.env.ACCESS_TOKEN, 
   captureUncaught: true,
-  captureUnhandledRejections: true
+  captureUnhandledRejections: true,
 });
 
 // record a generic message and send it to Rollbar
@@ -16,6 +19,8 @@ rollbar.log("Malo e lelei!");
 const app = express()
 
 app.use(express.json)
+
+
 
 app.get('/', function(req, res) {
     res.sendFile(path.join(__dirname, '../index.html'))
